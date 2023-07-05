0c5f680756ca388cf8ff830512b1422b814a2932
SanketZeple11 <120799518+SanketZeple11@users.noreply.github.com>
2023-07-05 11:30:27 +0530

    Update wsgi.py

--- Userapp/wsgi.py
+++ Userapp/wsgi.py
@@ -1,9 +1,9 @@
-from Userapp import app
-print("FEATURE")
-print("new")
-if __name__ == "__main__":
-    app.run()
+# from Userapp import app
+# print("FEATURE")
+# print("new")
+# if __name__ == "__main__":
+#     app.run()
 
-
+print("nice")
 
 
